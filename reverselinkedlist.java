180658f74c4253801f1c3965fcf1af841648acf9
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-09-05 12:48:32 -0700

    reverselinkedlist

---
+++ reverselinkedlist.java
@@ -0,0 +1,28 @@
+//Problem: Reverse a singly linked list.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+        
+    }
+}
