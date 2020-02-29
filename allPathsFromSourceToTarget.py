3411a434e50ce2af80fbbe00bb71ddfb351f6a7c
Nick Hsiao <hsiao.nicholas@gmail.com>
2020-02-28 16:10:05 -0800

    graph traversal problem

---
+++ allPathsFromSourceToTarget.py
@@ -0,0 +1,15 @@
+def allPathsSourceTarget(graph):
+    res = []
+    last = len(graph) - 1
+    def dfs(path,current):
+        if current == last:
+            res.append(path)
+            return
+        else:
+            for v in graph[current]:
+                dfs(path + [v],v)
+           
+    dfs([0],0)
+    return res
+    
+print(allPathsSourceTarget([[1,2],[3],[3],[]]))
