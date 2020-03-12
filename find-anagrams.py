fa39dcf40539e70b06c091fd20a02b27d9789226
Nick Hsiao <hsiao.nicholas@gmail.com>
2020-03-11 23:30:52 -0700

    finished anagram problem

--- find-anagrams.py
+++ find-anagrams.py
@@ -1,23 +1,31 @@
 import collections
 
 def findAnagrams(s, p):
+        if len(p) > len(s):
+            return []
+        
         c = collections.Counter(p)
         
-        l,counter, res, start =len(p), len(p), [], 0
-        for i,ch in enumerate(s):
-            if ch in c and c[ch] > 0:
-                c[ch] -= 1
-                counter -= 1
+        l, res, start, end =len(p), [], 0, 0
+        while end < len(s):
+            while end - start < l:
+                if s[end] in c:
+                    c[s[end]] -= 1
+                end += 1
             
-            if counter == 0:
-                res.append(i-l+1)
+            for v in c.values():
+                if v != 0:
+                    break
+            else:
+                res.append(start)
             
-            if i-start == l-1:
-                if s[start] in c:
-                    c[s[start]] += 1
-                    counter += 1
-                start += 1
+            if s[start] in c:
+                c[s[start]] += 1
+                
+            start += 1
         
         return res
+                
+                
 
 print(findAnagrams("cbaebabacd","abc"))
